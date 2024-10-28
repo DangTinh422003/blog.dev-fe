cc7e8df7ed3cc4b521ebc095446e8e864314af51
Cao Dang Tinh <97308298+DangTinh422003@users.noreply.github.com>
2024-10-28 14:37:58 +0700

    update: root layout (#2)

---
+++ src/components/layouts/DesktopMenu.tsx
@@ -0,0 +1,16 @@
+const DesktopMenu = () => {
+  return (
+    <div
+      className={`
+        fixed left-0 top-16 hidden h-screen overflow-y-auto overflow-x-hidden
+        bg-blue-500
+
+        lg:w-60
+
+        md:block md:w-16
+      `}
+    ></div>
+  )
+}
+
+export default DesktopMenu
---
+++ src/components/layouts/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div className={`
+      sticky left-0 top-0 z-20 flex h-16 w-full justify-between bg-red-500
+    `}>
+      Header
+    </div>
+  )
+}
+
+export default Header
