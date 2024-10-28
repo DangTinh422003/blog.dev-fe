f3b26b66ad46be408dfc7bef244a684477e4858f
Dang Tinh <caodangtinh04022003@gmail.com>
2024-10-28 14:24:56 +0700

    update: root layout

---
+++ src/components/layouts/MobileMenu.tsx
@@ -0,0 +1,13 @@
+const MobileMenu = () => {
+  return (
+    <div className={`
+      fixed bottom-0 left-0 w-full bg-yellow-400
+
+      md:hidden
+    `}>
+      MobileMenu
+    </div>
+  )
+}
+
+export default MobileMenu
